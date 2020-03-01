baf7d82d49fdea7f833dab9eeb6f3b962394fcf5
daro028 <31300422+daro028@users.noreply.github.com>
2020-03-01 20:21:40 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,41 +5,75 @@ $.get("https://sheetdb.io/api/v1/knwvt52fkyclp", function(data) {
   usersData = data;
 });
 
-$("input").keyup(function() {
+var search = function() {
   var inputData = $("input")
     .val()
     .toLowerCase();
-  results = usersData.filter(function(item) {
-    if (item.Usuario.toLowerCase().includes(inputData)) {
-      return true;
+  var curso = $("select").val();
+  if (inputData.length > 0) {
+    results = usersData.filter(function(item) {
+      if (
+        item.Usuario.toLowerCase().includes(inputData) &&
+        (curso != 0 ? item.Curso == curso : true)
+      ) {
+        return true;
+      }
+      if (
+        item.Apellido.toLowerCase().includes(inputData) &&
+        (curso != 0 ? item.Curso == curso : true)
+      ) {
+        return true;
+      }
+      if (
+        item.Nombre.toLowerCase().includes(inputData) &&
+        (curso != 0 ? item.Curso == curso : true)
+      ) {
+        return true;
+      }
+    });
+
+    console.log(results);
+
+    if (results.length == 0) {
+      $("#noresults").addClass("show");
+    } else {
+      $("#noresults").removeClass("show");
     }
-    if (item.Nombre.toLowerCase().includes(inputData)) {
-      return true;
-    }   
-     if (item.Apellido.toLowerCase().includes(inputData)) {
-      return true;
-    } 
-  });
+    $("#resultList").loadTemplate(
+      $("#resultsTemplate"),
+      results.map(function(item) {
+        return {
+          user: item.Usuario,
+          pass: item.Contrasena,
+          name: item.Nombre,
+          lastname: item.Apellido
+        };
+      })
+    );
+  } else {
+    results = [];
+    $("#resultList").loadTemplate(
+      $("#resultsTemplate"),
+      results.map(function(item) {
+        return {
+          user: item.Usuario,
+          pass: item.Contrasena,
+          name: item.Nombre,
+          lastname: item.Apellido
+        };
+      })
+    );
+  }
 
   if (!firstSearch) {
     $("body").addClass("results");
   }
+};
 
-  if (results.length == 0) {
-    $("#noresults").addClass("show");
-  } else {
-    $("#noresults").removeClass("show");
-  }
+$("input").keyup(function() {
+  search();
+});
 
-  $("#resultList").loadTemplate(
-    $("#resultsTemplate"),
-    results.map(function(item) {
-      return {
-        user: item.Usuario,
-        name: item.Nombre,
-        lastname: item.Apellido,
-        pass: item.Contrasena,
-      };
-    })
-  );
+$("select").change(function() {
+  search();
 });
