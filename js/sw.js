fef8645e9c9d897863d85387373e27122ea35528
dario <daro028@gmail.com>
2020-08-14 00:38:49 -0300

    keys

--- js/sw.js
+++ js/sw.js
@@ -9,7 +9,7 @@ urlsToCache = [
 'json/manifest.json'
 ]
 
-self.addEventListener('install', e => {
+self.addEventListener('install', e =>{
     e.waitUntil(
     caches.open(CACHE_NAME)
         .then(cache =>{
@@ -24,21 +24,21 @@ self.addEventListener('activate', e => {
     const cacheWhitelist = [CACHE_NAME]
 
     e.waitUntil(
-        cache.key()
+        caches.keys
         .then(cachesNames => {
             cachesNames.map(cacheName =>{
                 if (cacheWhitelist.indexOf(cacheName)===-1) {
-                    return cache.delete(cacheName)
+                    return caches.delete(cacheName)
                 }
             })
         })
-        .then(() => self.ClientRectList.claim())
+        .then(() => self.clients.claim())
     )
 })
 
 self.addEventListener('fetch', e => {
     e.respondWith(
-        cache.match(e.request)
+        caches.match(e.request)
         .then (res => {
             if(res){
                 return res
